a1dba426b3dcca60f7ec58c852c2f25ab682d80f
Lalith Lakshya <lakshyapentakota@gmail.com>
2025-12-28 12:49:00 +0530

    tweaks func

--- week0/functions.c
+++ week0/functions.c
@@ -1,9 +1,10 @@
 #include <stddef.h>
 #include <stdio.h>
-#define MAXLINE 1000 /* maximum input of a line */
-#define LIMIT 20
 
-int inputLine(char line[], int maxline, int active);
+#define MAXLINE 1000 /* maximum input of a line */
+#define LIMIT 80
+
+int inputLine(char line[], int maxline);
 void copy(char to[], char from[]);
 void reverse_while(char s[], int len);
 void reverse_for(char s[], int len);
@@ -15,7 +16,7 @@ int main() {
     char longest[MAXLINE];
 
     max = 0;
-    while ((len = inputLine(line, MAXLINE, 1)) > 0) {
+    while ((len = inputLine(line, MAXLINE)) > 0) {
         if (len >= LIMIT) {
             printf("Line Greater than 80 chars: %s\n", line);
         }
@@ -30,50 +31,46 @@ int main() {
         printf("Longest: %s\n", longest);
 }
 
-int inputLine(char s[], int lim, int active) {
-    int c, i;
+int inputLine(char s[], int lim) {
+    int c, i = 0;
 
     // terminate if crosses limit / if EOF / if newline (\n) -> End of line
     for (i = 0; i < lim - 1 && ((c = getchar()) != EOF) && c != '\n'; ++i) {
         s[i] = c;
     }
 
-   // printf("Last Element: %c\n", s[--i]);
+/*    while (i < lim - 1 && (c = getchar()) != EOF && c != '\n') {
+        s[i++] = c;
+    } */
 
-    while (s[i] == ' ' || s[i] == '\t') {
-        if (i == 0)
-            break;
-        else
-            --i;
-    }
-    
-    if (c == '\n') {
-        s[i] = c;
-        ++i;
-    }
     s[i] = '\0';
 
-    if (active) {
-        for (int j = 0; j < i; ++j) {
-            if (s[j] == '\n')
-                printf("\\n");
-            if (s[j] == ' ')
-                printf("\\b");
-            if (s[j] == '\t')
-                printf("\\t");
-            if (s[j] == '\0')
-                printf("\\0");
-            printf("%c", s[j]);
-        }
+    int j = i - 1;
+    while (j >= 0 && (s[j] == ' ' || s[j] == '\t')) {
+        if (s[j] == ' ')
+            printf("\nAt: %d -> \\b\n", j);
+        else if (s[j] == '\t')
+            printf("\nAt: %d -> \\t\n", j);
+        else if (s[j] == '\n')
+            printf("\nAt: %d -> \\n\n", j);
+        else if (s[j] == '\0')
+            printf("\nAt: %d -> \\0\n", j);
+        else
+            printf("At: %d -> %c", j, s[j]);
+        
+        --j;
     }
-    return i; /* Length of the string */
+
+    s[j + 1] = '\0';
+
+    return j + 1; /* Length of the string */
 }
 
 void copy(char to[], char from[]) {
     int i;
 
     i = 0;
-    while ((to[i] = from[i]) != '\0') /* condition gets checked from right to left */
+    while ((to[i] = from[i]) != '\0')
         ++i;
 }
 
